b9fc7a51c238f003c8fd0728df8949d9d43d28f9
Ikbenhetweetje <86172180+Ikbenhetweetje@users.noreply.github.com>
2021-06-20 01:18:49 +0200

    Add files via upload

---
+++ Bot host.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 15 23:01:48 2021
+
+@author: Simon
+"""
+
+import os
+import discord
+
+client = discord.Client()
+
+channelID = 854502157806338078
+
+TOKEN = os.getenv("Token")
+
+@client.event
+async def on_message(message):
+    
+    if message.channel.id == channelID:
+        
+        userID = message.author.id
+        
+        channel = client.get_channel(channelID)
+        
+        messageID = []
+        
+        async for message in channel.history():
+            if message.author.id == userID:
+                messageID.append(message.id)
+        
+        if len(messageID) > 1.5:
+            msg = await channel.fetch_message(messageID[1])
+            await msg.delete()
+
+
+
+client.run(TOKEN)
